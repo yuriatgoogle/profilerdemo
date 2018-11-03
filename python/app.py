e6a1614db9e6cf5235293c42d903382f89622c70
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-11-03 10:00:12 -0700

    python delay works

---
+++ python/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import logging
+import random
+import string
+import time
+import datetime
+app = Flask(__name__)
+
+def blockCPU(delay):
+    logging.warn("delaying for " + str(delay))
+    startTime = datetime.datetime.now()
+    endTime = startTime + datetime.timedelta(seconds = delay)
+    result = 0
+    logging.warn("start time is " + str(startTime))
+    logging.warn("end time is " + str(endTime)) 
+    while True: 
+        result += random.random() * random.random()
+        if (datetime.datetime.now() > endTime):
+            logging.warn("exiting loop")
+            return
+ 
+ 
+
+@app.route('/')
+def doProfile():
+    # generate random number to delay for:
+    blockCPU(5)
+    return "CPU blocked!"
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
